233c88255b20d4b735862dde9bcb8a9d4f00acfc
HongNyungKi <hnk2017@naver.com>
2020-07-20 18:59:42 +0900

    [add]js:navbar menu click auto scrol

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+//navbar 조절크기 조절
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -10,3 +12,20 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark')
     }
 });
+
+//메뉴선택시 해당메뉴로 스크롤자동 이동하기
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return
+    } else {
+        console.log(event.target.dataset.link);
+        const scrollTo = document.querySelector(link);
+        scrollTo.scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+})
